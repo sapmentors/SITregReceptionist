9a37c17b766d135ec06e6671a29e5b706427f1b8
gregorwolf <gregor.wolf@gmail.com>
2016-10-05 08:36:45 +0000

    Better formatting for location and date

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -2,7 +2,23 @@ sap.ui.define([
 	], function () {
 		"use strict";
 
+
 		return {
+			/**
+			 * Return the Registration Numbers for the Event
+			 *
+			 * @public
+			 * @param {int} iMaxParticipants Number of maximum participants
+			 * @param {int} iParticipants Number of current participants
+			 * @param {int} iFree Number of free places
+			 * @returns {string} Registration Numbers
+			 */
+			registrationNumbers : function (iMaxParticipants, iParticipants, iFree) {
+				if(iMaxParticipants !== null) {
+			    	var oResourceBundle = this.getModel("i18n").getResourceBundle();
+			    	return oResourceBundle.getText("masterRegistrationNumbers", [iFree, iParticipants, iMaxParticipants]);
+				}
+			},
 			/**
 			 * Rounds the currency value to 2 digits
 			 *
