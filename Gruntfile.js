00069d358c24dfdda6935d5debcb1c033fac21f5
gregorwolf <gregor.wolf@gmail.com>
2018-04-29 18:52:49 +0000

    Change back to pipeline template

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
-module.exports = function(grunt) {
-            "use strict";
-            grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-            grunt.registerTask("default", [
-                        "clean",
-                        "lint",
-                        "build"
-            ]);
+module.exports = function (grunt) {
+    'use strict';
+    grunt.loadNpmTasks('@sap/grunt-sapui5-bestpractice-build');
+
+    grunt.registerTask('default', [
+        'lint',
+        'clean',
+        'build'
+    ]);
 };
